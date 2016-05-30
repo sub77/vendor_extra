f16f5f55bfa8dca7da176af57af5c8d781749d9c
ThrillerAtPlay <thrilleratplay@gmail.com>
2016-05-30 21:55:50 +0000

    Add Snap

--- product.mk
+++ product.mk
@@ -13,7 +13,8 @@ include frameworks/opt/slim/slim_framework.mk
 # Add wanted packages
 PRODUCT_PACKAGES += \
 		FDroid \
-		Matlog
+		Matlog \
+		Snap
 
 ########################################
 ############# Settings #################
@@ -23,7 +24,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES := $(subst dataroaming=false,dataroaming=true,$(PRODUCT_PROPERTY_OVERRIDES))
 
 # Disable multiuser
-PRODUCT_PROPERTY_OVERRIDES += fw.show_multiuserui=0
+PRODUCT_PROPERTY_OVERRIDES += \
+		fw.show_multiuserui=0
 
 # Updates overlay settings
 PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay/common
