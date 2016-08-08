f48bc1d89e8244ac6852c5d026ac6bb12cb2c3a7
Steve Kondik <steve@cyngn.com>
2016-08-07 23:21:29 -0700

    cm: Add custom off-mode charging screen
      * Moving this to open-source from CyanogenOS :)
      * Thanks to Chao Chen, Ethan Chen, Pat Erley, Scott Mertz,
        and Keith Mok for various contributions to this.
    
    Change-Id: If79f9c279668d14ee0cde62889bb09e7185ef08c
    Signed-off-by: sub77 <sub77@ymail.com>

--- product.mk
+++ product.mk
@@ -31,3 +31,12 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
+
+# Custom off-mode charger
+ifneq ($(WITH_CM_CHARGER),false)
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    cm_charger_res_images \
+    font_log.png \
+    libhealthd.cm
+endif
