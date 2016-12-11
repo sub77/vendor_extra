2d8754879c463cebca3639d6cb5af4ab51db06e0
sub77 <sub77@ymail.com>
2016-12-11 14:12:01 +0000

    Prebuilt Product Package: Fix Module path

--- prebuilt/common/apk/Android.mk
+++ prebuilt/common/apk/Android.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := Mixplorer
 LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
