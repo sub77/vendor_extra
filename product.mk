49824693c576bc004aabddcf2dfe36b6b538d04f
sub77 <sub77@ymail.com>
2016-12-11 13:03:37 +0000

    Product Package: OpenDelta
    Signed-off-by: sub77 <sub77@ymail.com>

--- product.mk
+++ product.mk
@@ -6,11 +6,15 @@
 PRODUCT_COPY_FILES += \
 		vendor/extra/products/common/rootdir/adbkey.pub:root/adb_keys
 
+# Add prebuilt packages
+PRODUCT_PACKAGES += \
+		Mixplorer \
+		Substratum
+
 # Add wanted packages
 PRODUCT_PACKAGES += \
 		LockClock \
-		Mixplorer \
-		Substratum
+		OpenDelta
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
