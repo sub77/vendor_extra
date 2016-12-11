8ce3c5fd31ef8ff090e13fe1c2d74a93dd6825e6
sub77 <sub77@ymail.com>
2016-12-11 23:07:01 +0000

    Move bootanimation to vendor_extra

--- product.mk
+++ product.mk
@@ -4,23 +4,27 @@
 
 # ADB
 PRODUCT_COPY_FILES += \
-		vendor/extra/products/common/rootdir/adbkey.pub:root/adb_keys
+    vendor/extra/products/common/rootdir/adbkey.pub:root/adb_keys
 
 # Add prebuilt packages
 PRODUCT_PACKAGES += \
-		Mixplorer \
-		Substratum
+    Mixplorer \
+    Substratum
 
 # Add wanted packages
 PRODUCT_PACKAGES += \
-		LockClock \
-		OpenDelta
+    LockClock \
+    OpenDelta
 
 # Boot Animation
-DU_CUSTOM_BOOTANIMATION := true
-
-PRODUCT_COPY_FILES += \
-		vendor/extra/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
+ifeq (du_falcon,$(TARGET_PRODUCT))
+    PRODUCT_COPY_FILES += \
+        vendor/extra/prebuilt/falcon/media/bootanimation.zip:system/media/bootanimation.zip
+endif
+ifneq (du_falcon,$(TARGET_PRODUCT))
+    PRODUCT_COPY_FILES += \
+        vendor/extra/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
+endif
 
 ########################################
 ############# Settings #################
