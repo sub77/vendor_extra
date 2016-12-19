9f0ca16f5159fa8ed8d6ba21cffcd88ceffa23b2
sub77 <sub77@ymail.com>
2016-12-19 21:32:49 +0100

    Initial twrp extra vendor

--- product.mk
+++ product.mk
@@ -1,32 +1,3 @@
-#TODO: Copy resolv.conf
-
 ########################################
 ############## Packages ################
 ########################################
-
-# Add wanted packages
-PRODUCT_PACKAGES += \
-		FDroid \
-		Matlog \
-		Snap \
-		LayersManager \
-		LockClock \
-		htop
-
-########################################
-############# Settings #################
-########################################
-
-# Enable data roaming
-PRODUCT_PROPERTY_OVERRIDES := $(subst dataroaming=false,dataroaming=true,$(PRODUCT_PROPERTY_OVERRIDES))
-
-# Disable multiuser
-PRODUCT_PROPERTY_OVERRIDES += \
-		fw.show_multiuserui=0
-
-# Enable Root for adb and apps
-PRODUCT_PROPERTY_OVERRIDES += \
-		persist.sys.root_access=3
-
-# Updates overlay settings
-PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay/common
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo omni_falcon-eng
+. ./vendor/extra/build/fmsetup.sh
