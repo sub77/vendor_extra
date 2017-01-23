b71591cc3707b8e9fae15eb96b8653a7480b94ee
sub77 <sub77@ymail.com>
2017-01-23 21:42:44 +0000

    update

--- caf-vendorsetup.sh
+++ caf-vendorsetup.sh
@@ -1,7 +1,4 @@
-source ./vendor/extra/config.sh
-source ./vendor/extra/build/colors.sh
-source ./vendor/extra/build/ccache.sh
-source ./vendor/extra/build/repos.sh
-source ./vendor/extra/patch/base/apply.sh
+. "$(gettop)/vendor/extra/config.sh"
+. "$(gettop)/vendor/extra/build/envsetup.sh"
 
 add_lunch_combo du_falcon-userdebug
--- config.sh
+++ config.sh
@@ -1,3 +1,9 @@
 ccache_use=1
-ccache_dir=/1and/ccache
+ccache_dir=~/ccache
 ccache_size=15G
+
+func_alias
+func_colors
+func_ccache
+func_java
+func_repos
