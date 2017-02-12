0ca969c471cab6613fefa5420b43bc8ead0e60da
sub77 <sub77@ymail.com>
2017-02-12 08:24:16 +0100

    Show seliux status in settings

--- product.mk
+++ product.mk
@@ -11,3 +11,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/extra/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
     vendor/extra/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
