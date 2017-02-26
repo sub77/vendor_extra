14acba4e425ccfc035d56d2435d59408cc5e5951
sub77 <sub77@ymail.com>
2017-02-27 00:58:19 +0100

    update

---
+++ build/config.sh
@@ -0,0 +1,10 @@
+ccache_use=1
+ccache_dir=/home/sub77/ccache
+ccache_size=5
+
+rom_type=omni # cm/du/omni
+
+#with_su=1
+
+have_sdclang=1
+path_sdclang="vendor/extra/toolchain/sdclang-3.8/bin"
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,3 @@
-. "$(gettop)/vendor/extra/config.sh"
+. "$(gettop)/vendor/extra/build/config.sh"
+. "$(gettop)/vendor/extra/build/functions.sh"
 . "$(gettop)/vendor/extra/build/envsetup.sh"
