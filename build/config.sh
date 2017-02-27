f9219ef1b1d133213f4a50ad2e167e21d5b1fdda
sub77 <sub77@ymail.com>
2017-02-27 01:17:31 +0100

    init

---
+++ build/config.sh
@@ -0,0 +1,5 @@
+ccache_use=1
+ccache_dir=/home/sub77/ccache
+ccache_size=25G
+java_home=/usr/lib/jvm/java-7-openjdk
+rom_type=du # cm/du/omni
---
+++ build/envsetup.sh
@@ -0,0 +1,8 @@
+func_colors
+func_setenv
+func_java
+func_ccache
+#func_toolchain
+func_alias
+#func_twrp
+#func_repos
