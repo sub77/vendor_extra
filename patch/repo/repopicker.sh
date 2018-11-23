5010527fb24d3284b95aaca6a6891030384e1440
sub77 <sub77@ymail.com>
2018-11-23 08:58:12 +0100

    patch: update

--- patch/repo/repopicker.sh
+++ patch/repo/repopicker.sh
@@ -1,11 +1,11 @@
 repopick_omnirom="./vendor/omni/build/tools/repopick.py"
 
 # topic:flex-9.0
-echo -e ${rst}${bldylw} x ${rst}${bldblu}
-$repopick_omnirom 32664
+#echo -e ${rst}${bldylw} x ${rst}${bldblu}
+#$repopick_omnirom 32664
 
-echo -e ${rst}${bldylw} x ${rst}${bldblu}
-$repopick_omnirom 32665
+#echo -e ${rst}${bldylw} x ${rst}${bldblu}
+#$repopick_omnirom 32665
 
 # topic:wifi-sta-ap-concurrency
 echo -e ${rst}${bldylw} x ${rst}${bldblu}
@@ -24,8 +24,8 @@ echo -e ${rst}${bldylw} x ${rst}${bldblu}
 $repopick_omnirom 32602
 
 # misc
-echo -e ${rst}${bldylw} "bionic: hack for pthread mutex bug for old camera blob" ${rst}${bldblu}
-$repopick_omnirom 32643
+#echo -e ${rst}${bldylw} "bionic: hack for pthread mutex bug for old camera blob" ${rst}${bldblu}
+#$repopick_omnirom 32643
 
 echo -e ${rst}${bldylw} "omni: add hours and minutes to ro.omni.version" ${rst}${bldblu}
 $repopick_omnirom -f 32545
